da961a8629a3c3172e9dd36e9270e2993a35bab1
thanhcongnguyen <thanhcong150593@gmail.com>
2019-12-30 23:50:08 +0700

    add product

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors());
 
 
 //use routes
-app.use('api/user', usersRouter);
-app.use('api/provider', providersRouter);
-app.use('api/token', authRouter);
+app.use('/user', usersRouter);
+app.use('/provider', providersRouter);
+app.use('/token', authRouter);
 module.exports = app;
